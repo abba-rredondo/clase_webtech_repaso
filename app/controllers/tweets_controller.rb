30dfd41a7b5011908e86ec5a0b8fd07359ff927e
webtech <webtech@webtech-debian.webtech>
2023-06-10 16:35:19 -0400

    El new funcionando
    Loco me equivoqué y puse una d extra y en verdad, terrible

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -8,5 +8,33 @@ class TweetsController < ApplicationController
         @tweet= Tweet.find params[:id]
 
     end
+
+    def new
+        @tweet = Tweet.new
+    end
+
+    def create #ES CREATE NO CREATED
+        tweet= Tweet.new tweet_params #SOlo va a aceptar los parametros que yo señalo, por terminso de seguridad
+        tweet.user = current_user #Porque necesita un usuario al que estar asociado para poder guardarse
+        if tweet.save
+            redirect_to tweet, notice: 'Tweet guardado con éxito'
+        else
+            render :new
+    
+        end
+    end
+
+    private
+    def tweet_params
+        params.require(:tweet).permit(:body) #Esto me dice, solo te acepto un body, lo que es IMPORTANTISIMO, debido a que si no, 
+        #pueden meterme otras cosas en el campo y hackearme porque pueden meter comandos
+    end
+
+    # def destroy
+    #     tweet = Tweet.find params[:id] #Antes de eliminar un tweet necesito saber el usuario
+    #     tweet.destroy
+
+
+    # end
     
 end
