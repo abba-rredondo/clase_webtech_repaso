6f9153e12b750c3c66c828191da0aa80d1e3f2a8
webtech <webtech@webtech-debian.webtech>
2023-06-10 17:28:12 -0400

    haciendo la tabla intermedia

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,6 @@
 class Tweet < ApplicationRecord
     belongs_to :user
+    has_many :tags
     def creation_date
         created_at.strftime("%e %b %Y") #Busqué en google el strftime y me da todas las opciones de formato, si quiero con / la fecha
         #es lo mismo pero en vez de espacio es /
