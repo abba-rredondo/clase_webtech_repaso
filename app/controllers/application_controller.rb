b1a2aabc45864e1593cb918534488774d72408da
webtech <webtech@webtech-debian.webtech>
2023-06-10 13:55:22 -0400

    feat: agregar botón log out y permite registrarse con nombre y apellido
    loco hay que acordarse de cerrar los tags para que funcione esto jsnnjd

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name,:last_name]) #ESto es para que device si me mande los datos que pongo a la base de datos para poder usarlos
+  end
 end
